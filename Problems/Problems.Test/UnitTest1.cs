1ad2df57675244492e372b500652ed580d55b3d4
Keegan Lubbe <g16L3152@ict.ru.ac.za>
2020-02-20 16:22:23 +0200

    added tests and stuff

--- Problems/Problems.Test/UnitTest1.cs
+++ Problems/Problems.Test/UnitTest1.cs
@@ -12,7 +12,9 @@ namespace Problems.Test
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            Assert.AreEqual("5,1:hello", Prefix("hello"));
+            Assert.AreEqual("0,0:", Prefix(""));
+            Assert.AreEqual("27,5:what ... did you say?? ", Prefix("what ... did you say?? "));
         }
     }
 }
--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,49 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+        String Prefix(string input)
+        {
+            bool whitespace = false;
+            int wordcount = 0;
+            int charcount = 0;
+            int iLength = input.Length;
+            while(charcount < iLength)
+            {   
+                if((charcount == 0) && (input[charcount] != ' '))
+                {
+                    charcount++;
+                    wordcount++;
+                }
+                if(whitespace == true)
+                {
+                    if(input[charcount] == ' ')
+                    {
+                        charcount++;
+                    }
+                    else
+                    {
+                        wordcount++;
+                        charcount++;
+                        whitespace = false;
+                    }
+                }
+                else
+                {
+                    if(input[charcount] == ' ')
+                    {
+                        whitespace = true;
+                    }
+                    charcount++;
+                }
+                
+            }
+            return (charcount.ToString() + "," + wordcount.ToString() + ":" + input);
         }
     }
 }
